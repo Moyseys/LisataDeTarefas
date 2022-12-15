b0f7b70545e6f66a07ba5a4120dad71d43588749
Moyseys Ferreira Veroni <moyseys.veroni@gmail.com>
2022-12-15 13:24:21 -0300

    1.0

---
+++ assets/js/main.js
@@ -0,0 +1,71 @@
+let inputTarefa = document.querySelector('.input-tarefa')
+let btnTarefa = document.querySelector('.btn-tarefa')
+let tarefa = document.querySelector('.tarefas')
+
+adicionaTarefaSalva()
+
+inputTarefa.addEventListener('keypress',(e) => {
+  if(!inputTarefa.value) return
+  let vinput = inputTarefa.value 
+  if(e.keyCode === 13) criaTarefa(vinput)
+})
+
+btnTarefa.addEventListener('click', () => {
+  if(!inputTarefa.value) return
+  let vinput = inputTarefa.value 
+  criaTarefa(vinput)
+})
+
+
+function limpaInput(){
+  inputTarefa.value = ''
+  inputTarefa.focus()
+}
+
+function criaBtnApagar(li){
+  li.innerHTML += ' '
+  const btnApagar = document.createElement('button')
+  li.appendChild(btnApagar)
+  btnApagar.innerText = 'Apagar'
+  btnApagar.classList.add('btnapaga')
+}
+
+function criaTarefa(vl){
+    let elementoLi = document.createElement('li')
+    let liText = document.createTextNode(vl)
+    elementoLi.appendChild(liText)
+    tarefa.appendChild(elementoLi)
+    criaBtnApagar(elementoLi)
+    limpaInput()
+    salvarTarefas()
+}
+
+document.addEventListener('click', (e) => {
+  let el = e.target
+  if(el.classList.contains('btnapaga')){
+    el.parentElement.remove()
+    salvarTarefas()
+  }
+})
+
+function salvarTarefas(){
+  let lis = tarefa.querySelectorAll('li')
+  let textLiList = []
+  for(let tarefas of lis){
+    let textTarefa = tarefas.innerText
+    textTarefa = textTarefa.replace('Apagar',' ').trim() //remove os espaços
+    textLiList.push(textTarefa)
+  }
+  const tarefasJson = JSON.stringify(textLiList)
+  localStorage.setItem('tarefas', tarefasJson)
+  
+}
+
+function adicionaTarefaSalva(){
+  const tarefas = localStorage.getItem('tarefas')
+  const listaTarefa = JSON.parse(tarefas)
+
+  for(let tarefa of listaTarefa){
+    criaTarefa(tarefa)
+  }
+}
