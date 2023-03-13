0feda44cd270b21ae4eb2544007c45542a246d07
Moyseys Ferreira Veroni <moyseys.veroni@gmail.com>
2023-03-13 17:10:52 -0300

    classes js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,98 +1,107 @@
-let inputTarefa = document.querySelector('.input-tarefa')
-let btnTarefa = document.querySelector('.btn-tarefa')
-let tarefa = document.querySelector('.tarefas')
-let retorno = document.querySelector('.retorno')
+const inputTarefa = document.querySelector('.input-tarefa')
+const btnTarefa = document.querySelector('.btn-tarefa')
+const tarefa = document.querySelector('.tarefas')
+const retorno = document.querySelector('.retorno')
 
-if(localStorage.key(0) != null) adicionaTarefaSalva()
 
-inputTarefa.addEventListener('keypress',(e) => {
-  if(!inputTarefa.value) return
-  let vinput = inputTarefa.value 
-  if(e.keyCode === 13) criaTarefa(vinput)
-})
-
-document.addEventListener('keypress', (e) =>{
-  if(e.ctrlKey || e.metaKey && e.key === 'z') desfazer()
-})
-
-btnTarefa.addEventListener('click', () => {
-  if(!inputTarefa.value) return
-  let vinput = inputTarefa.value 
-  criaTarefa(vinput)
-})
-
-retorno.addEventListener('click', () => {
-  try {
-    if(localStorage.key(0) !== null ){
-      desfazer()
-      return
-    } 
-  } catch (error) {
+class Lista{
+  acaoApaga(e){
+    let el = e.target
+    if(el.classList.contains('btnapaga')){
+      el.parentElement.remove()
+      this.salvarTarefas()
+    }
   }
-})
+  desfazer(){
+    //remove el
+    const tarefasList = document.querySelectorAll('li.tarefa')
+    const itemRemover = tarefasList[tarefasList.length  - 1]
+    itemRemover.remove()
+  
+    //remove do localstorage
+    const  tarefasSalvas = JSON.parse(localStorage.getItem('tarefas'))
+    tarefasSalvas.splice(-1, 1)
+    this.salvarTarefas()  
+  }
 
-function desfazer(){
-  //remove el
-  const tarefasList = document.querySelectorAll('li.tarefa')
-  const itemRemover = tarefasList[tarefasList.length  - 1]
-  itemRemover.remove()
+  limpaInput(){
+    inputTarefa.value = ''
+    inputTarefa.focus()
+  }
 
-  //remove do localstorage
-  const  tarefasSalvas = JSON.parse(localStorage.getItem('tarefas'))
-  tarefasSalvas.splice(-1, 1)
-  salvarTarefas()  
-}
+  criaBtnApagar(li){
+    li.innerHTML += ' '
+    const btnApagar = document.createElement('button')
+    li.appendChild(btnApagar)
+    btnApagar.innerText = 'Apagar'
+    btnApagar.classList.add('btnapaga')
+  }
 
-function limpaInput(){
-  inputTarefa.value = ''
-  inputTarefa.focus()
-}
-
-function criaBtnApagar(li){
-  li.innerHTML += ' '
-  const btnApagar = document.createElement('button')
-  li.appendChild(btnApagar)
-  btnApagar.innerText = 'Apagar'
-  btnApagar.classList.add('btnapaga')
-}
-
-function criaTarefa(vl){
+  criaTarefa(vl){
     let elementoLi = document.createElement('li')
     let liText = document.createTextNode(vl)
     elementoLi.appendChild(liText)
     elementoLi.classList.add('tarefa')
     tarefa.appendChild(elementoLi)
-    criaBtnApagar(elementoLi)
-    limpaInput()
-    salvarTarefas()
-}
-
-document.addEventListener('click', (e) => {
-  let el = e.target
-  if(el.classList.contains('btnapaga')){
-    el.parentElement.remove()
-    salvarTarefas()
+    this.criaBtnApagar(elementoLi)
+    this.limpaInput()
+    this.salvarTarefas()
   }
-})
 
-function salvarTarefas(){
-  let lis = tarefa.querySelectorAll('li')
-  let textLiList = []
-  for(let tarefas of lis){
-    let textTarefa = tarefas.innerText
-    textTarefa = textTarefa.replace('Apagar',' ').trim() //remove os espaços
-    textLiList.push(textTarefa)
+  salvarTarefas(){
+    let lis = tarefa.querySelectorAll('li')
+    let textLiList = []
+    for(let tarefas of lis){
+      let textTarefa = tarefas.innerText
+      textTarefa = textTarefa.replace('Apagar',' ').trim() //remove os espaços
+      textLiList.push(textTarefa)
+    }
+    const tarefasJson = JSON.stringify(textLiList)
+    localStorage.setItem('tarefas', tarefasJson)
+    
   }
-  const tarefasJson = JSON.stringify(textLiList)
-  localStorage.setItem('tarefas', tarefasJson)
   
-}
+  adicionaTarefaSalva(){
+    const tarefas = localStorage.getItem('tarefas')
+    const listaTarefa = JSON.parse(tarefas)
+  
+    for(let tarefa of listaTarefa){
+      this.criaTarefa(tarefa)
+    }
+  }
+  
+  enter(e){
+    if(!inputTarefa.value) return
+    if(e.keyCode === 13) this.criaTarefa(inputTarefa.value )
+  }
 
-function adicionaTarefaSalva(){
-  const tarefas = localStorage.getItem('tarefas')
-  const listaTarefa = JSON.parse(tarefas)
+  ctrlz(e){
+    if(e.ctrlKey || e.metaKey && e.key === 'z') this.desfazer()
+  }
 
-  for(let tarefa of listaTarefa){
-    criaTarefa(tarefa)
+  chamaTarefa(e){
+    if(!inputTarefa.value) return
+    let vinput = inputTarefa.value 
+    this.criaTarefa(vinput)
+  }
+
+  retorno(e){
+    try {
+      if(localStorage.key(0) !== null ){
+        this.desfazer()
+        return
+      } 
+    } catch (error){
+      alert('Não ha itens para excluir')
+    }
   }
 }
+const lista = new Lista()
+if(localStorage.key(0) != null) lista.adicionaTarefaSalva()
+
+
+inputTarefa.addEventListener('keypress', e => lista.enter(e))
+document.addEventListener('keypress', e => lista.ctrlz(e))
+btnTarefa.addEventListener('click', e => lista.chamaTarefa(e))
+retorno.addEventListener('click', e => lista.retorno(e))
+document.addEventListener('click', e => lista.acaoApaga(e))
